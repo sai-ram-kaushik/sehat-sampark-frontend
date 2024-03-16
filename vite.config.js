47ed6e1f2c2be34e360d95f3c8f62eea257a2fcc
sai-ram-kaushik <sairamkaushik1@gmail.com>
2024-03-17 01:35:40 +0530

    updated with the vite.config,js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import { createProxy } from "vite-plugin-proxy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        createProxy({
-            "/api": {
-                target: "https://sehatsampark-backend.onrender.com",
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, ""),
-            },
-        }),
-    ],
     plugins: [react()],
 });
